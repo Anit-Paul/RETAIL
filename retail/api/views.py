ed709232c168cc213352d8c0a3582dccdd4fdecf
Anit Paul <anit42655@gmail.com>
2026-07-03 15:59:12 +0000

    token function added

--- retail/api/views.py
+++ retail/api/views.py
@@ -4,12 +4,28 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer, LoginSerializer
 from django.contrib.auth import authenticate
+#token
+from rest_framework_simplejwt.tokens import RefreshToken
+#Generate tokens manually for a user
+def get_tokens_for_user(user):
+    if not user.is_active:
+        raise AuthenticationFailed("User is not active")
+
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+    
+    
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user=serializer.save()
+            token=get_tokens_for_user(user)
+            return Response({"token": token, "message":"User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(APIView):
@@ -21,7 +37,8 @@ class LoginView(APIView):
             user = authenticate(request, email=email, password=password)
             if user is not None:
                 # Perform login and return a success response
-                return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+                token=get_tokens_for_user(user)
+                return Response({"token": token, "message":"Login successful"}, status=status.HTTP_200_OK)
             else:
                 return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
